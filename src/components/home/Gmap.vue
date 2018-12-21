e1e0277b0f85ab53dfbafb0b093be7c35058511d
Serhii Kovalenko <serhii.kovalenko@otakoyi.com>
2018-12-21 16:54:51 +0200

    wfef

---
+++ src/components/home/Gmap.vue
@@ -0,0 +1,42 @@
+<template>
+    <div rel="map"  class="map container" id="map">
+        <h2>Map</h2>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Gmap",
+    data() {
+      return {
+        lat: 53,
+        lng: -2,
+      }
+    },
+    methods: {
+      renderMap() {
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: this.lat, lng: this.lng},
+          zoom: 6,
+          maxZoom: 15,
+          minZoom: 3,
+          streetVievControl: false,
+        })
+      },
+    },
+    mounted() {
+      this.renderMap();
+    },
+  }
+</script>
+
+<style>
+#map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: -1;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Gmap from '@/components/home/Gmap'
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Gmap',
+      component: Gmap,
+      props: true,
     }
   ]
 })
